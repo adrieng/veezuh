df72580fda645e4b23f9e137b9c20abdbcb6587e
Adrien Guatto <adrien@guatto.org>
2017-05-01 11:08:10 -0400

    Remove example code from the lablgtk demo

--- src/veezuh.ml
+++ src/veezuh.ml
@@ -4,19 +4,10 @@ open GMain
 open GdkKeysyms
 open Timeline
 
-let font_name = "-*-dejavu sans-medium-*-*-*-*-*"
-
-let locale = GtkMain.Main.init ()
-
-let font =
-  try
-    Gdk.Font.load font_name
-  with
-    Gpointer.Null -> failwith ("graph.ml: font " ^ font_name ^ ": not found")
-
 let main ds =
-  let window = GWindow.window ~width:640 ~height:480
-                              ~title:"LablGtk graph widget demo" () in
+  ignore @@ GMain.init ();
+
+  let window = GWindow.window ~title:"Veezuh" () in
   let vbox = GPack.vbox ~packing:window#add () in
   ignore @@ window#connect#destroy ~callback:Main.quit;
 
@@ -29,13 +20,6 @@ let main ds =
   (* File menu *)
   let factory = new GMenu.factory file_menu ~accel_group in
   ignore @@ factory#add_item "Quit" ~key:_Q ~callback:Main.quit;
-
-  (* Data. *)
-  let array = Array.init 100 (fun _ -> Random.int 10) in
-
-  (* Create a graph in the main area. *)
-  let graph = new graph font ~packing:vbox#add array in
-  graph#set_title "Random data";
 
   (* Display the windows and enter Gtk+ main loop *)
   window#add_accel_group accel_group;
