80530950ee12e4d7c914455e23687109adad54fb
Adrien Guatto <adrien@guatto.org>
2017-05-03 10:50:47 -0400

    Open one window per command-line argument

--- src/veezuh.ml
+++ src/veezuh.ml
@@ -9,36 +9,27 @@ let height = 600
 
 let events =
   [
-    "Thread copying", "THREAD_COPY", (0.755, 0.8, 0.1, 0.8);
+    "Thread copying", "THREAD_COPY", (0.2, 0.3, 0.1, 0.8);
   ]
 
-let event_boxes = ref []
-
-let create_event_check_buttons (f : GMenu.menu GMenu.factory) =
-  let create_event_check_button (label, _, _) =
-    event_boxes :=
-      f#add_check_item
-        ~key:GdkKeysyms._T
-        "Thread creation"
-      :: !event_boxes
+let create_event_check_buttons tl (f : GMenu.menu GMenu.factory) =
+  let create_event_check_button (label, event_name, color) =
+    let cb = f#add_check_item ~key:GdkKeysyms._T "Thread creation" in
+    let callback () = tl#toggle_kind event_name color in
+    ignore @@ cb#connect#toggled ~callback
   in
   List.iter create_event_check_button events
-
-let connect_callbacks tl =
-  let connect_callback cb (_, event_name, color) =
-    ignore @@
-      cb#connect#toggled ~callback:(fun () -> tl#toggle_kind event_name color)
-  in
-  List.iter2 connect_callback !event_boxes events
 
 let create_timeline (b : GPack.box) trace =
   let vbox = GPack.vbox ~packing:(b#pack ~expand:true) () in
   ignore @@ GMisc.label ~markup:"<b><u>Timeline</u></b>" ~packing:vbox#pack ();
   new timeline ~packing:(vbox#pack ~expand:true ~fill:true) trace
 
-let main filename trace =
-  (* Initialize Gtk. DO NOT REMOVE! *)
-  ignore @@ GMain.init ();
+(* Initialize Gtk. DO NOT REMOVE! *)
+let _ = GMain.init ()
+
+let open_trace_window filename =
+  let trace = Trace.from_sqlite_file filename in
 
   (* Window *)
   let window = GWindow.window ~width ~height ~title:("Veezuh " ^ filename) () in
@@ -68,26 +59,19 @@ let main filename trace =
   ignore @@
     factory#add_item
       "Zoom to selection"
-      ~key:GdkKeysyms._V
+      ~key:GdkKeysyms._E
       ~callback:(fun _ -> tl#zoom_to_selection ());
 
-  create_event_check_buttons factory;
+  create_event_check_buttons tl factory;
 
   window#add_accel_group accel_group;
 
-  (* Connect the event buttons callback to the timeline. *)
-  connect_callbacks tl;
-
   (* Display the windows and enter Gtk+ main loop *)
   window#show ();
-  Main.main ()
+  ()
 
 let () =
-  let fn =
-    if Array.length Sys.argv < 2
-    then (Printf.eprintf "Usage: %s file.sqlite" Sys.argv.(0); exit 1)
-    else Sys.argv.(1)
-  in
-
-  let trace = Trace.from_sqlite_file fn in
-  main fn trace
+  for i = 1 to Array.length Sys.argv - 1 do
+    open_trace_window Sys.argv.(i)
+  done;
+  GMain.main ()
