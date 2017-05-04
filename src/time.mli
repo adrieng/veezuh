5dd1b7cfc107be638b9221ea5dd941f45d07b8e1
Adrien Guatto <adrien@guatto.org>
2017-05-04 12:06:28 -0400

    Implement horizontal scrolling

--- src/time.mli
+++ src/time.mli
@@ -2,6 +2,10 @@ type time = float
 
 type span = { l : time; u : time; }
 
+val print_span : Format.formatter -> span -> unit
+
 val truncate : span -> time -> time
+
+val clip : within:span -> span -> span
 
 val range : span -> time
